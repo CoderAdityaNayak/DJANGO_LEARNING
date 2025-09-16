6ec8987aecb81e0cf775f8f35ec5202671aceefe
CoderAdityaNayak <proaditya121@gmail.com>
2025-09-16 21:31:50 +0530

    FINAL CHANGES

--- app9/views.py
+++ app9/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from .forms import toolkit   # make sure toolkit form exists
+from .forms import toolkit 
 
 def home(request):
-    combined = []  # default empty, so template won't crash
+    combined = []  #empty list for our values
 
     if request.method == "POST":
         form1 = toolkit(request.POST)
         if form1.is_valid():
             data = form1.cleaned_data
             num1 = data.get("num1")
-            num2 = data.get("num2")
+            num2 = data.get("num2")      
 
-            num, sqr, cubes = [], [], []
+            num=[]
+            sqr=[]
+            cubes=[]
             for i in range(num1, num2+1,1):
                 num.append(i)
                 sqr.append(i*i)
@@ -21,4 +23,4 @@ def home(request):
     else:
         form1 = toolkit()  # empty form for GET requests
 
-    return render(request, 'app9/index.html', {'form': form1, 'param1': combined})
+    return render(request, 'app9/index.html', {'form': form1, 'data': combined})
